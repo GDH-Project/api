87db65108a8b1ec5138a40c12abfeed0af6126ff
SOPLAY <tkdgns1341@gmail.com>
2025-10-26 23:11:10 +0900

    fix: validate_user.go admin 프리패스 설정
    - ref #54

--- internal/util/validate_user.go
+++ internal/util/validate_user.go
@@ -10,6 +10,7 @@ import (
 // ValidateUser 유저 권한 검사
 //
 // auth middleware를 통과한 ctx(Ctx)를 넘기고 통과시킬 권한명(TargetRole)과 차단 권한명(ExceptionRole) 을 필요에 따라 설정하면 된다.
+// 만약 권한이 admin인 경우 무조건 통과한다.
 // 권한 평가의 우선 순위는 현재 권한을 불라 올수 없는 경우 > 차단 권한 > 통과 권한 의 우선순위를 가지고 있다.
 type ValidateUser struct {
 	Ctx           context.Context
@@ -40,6 +41,10 @@ func (c *ValidateUser) UserRole() string {
 // 권한 평가의 우선 순위는 현재 권한을 불라 올수 없는 경우 > 차단 권한 > 통과 권한 의 우선순위를 가지고 있다.
 func (c *ValidateUser) Exec() bool {
 	currentRole := c.UserRole()
+
+	if currentRole == string(domain.UserRoleAdmin) {
+		return true
+	}
 
 	if currentRole == "" || currentRole == string(c.ExceptionRole) {
 		return false
